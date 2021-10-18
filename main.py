e6927ced8798964b351f6fe61215da3e999d5530
Verevkina Elena <verevkinaee@gmail.com>
2021-10-18 14:22:47 +0300

    make changes to search.html, create app.route search

--- main.py
+++ main.py
@@ -18,5 +18,22 @@ def get_post(post_id):
     return render_template("post.html", post=post, comments=get_comments_by_post_id(post_id))
 
 
+@app.route('/search/')
+def search_post():
+    search_parameter = request.args.get("s")
+    if not search_parameter:
+        abort(400)
+    search_list = []
+    posts = get_posts()
+    for post in posts:
+        if len(search_list) == 10:
+            break
+        elif search_parameter.lower() in post["content"].lower():
+            search_list.append(post)
+    if search_list:
+        return render_template("search.html", search_list=search_list, search_parameter=search_parameter)
+    return abort(404)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
